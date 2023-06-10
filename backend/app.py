8fd40febf8c66d077ffc03992d27513c7d14c408
Chad Nuttall <chadnuttall1@gmail.com>
2023-06-10 17:51:34 -0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   backend/app.py
    	new file:   backend/models/__pycache__/__init__.cpython-311.pyc
    	new file:   backend/models/__pycache__/database.cpython-311.pyc
    	new file:   backend/models/__pycache__/dives.cpython-311.pyc
    	new file:   backend/models/database.py
    Setup the database connection in the models folder.
    Everything is currently functioning to get to the index and /test page

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,32 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.database import init_db
+from models.dives import Dive
 import os
-from sqlalchemy import text
+from dotenv import load_dotenv
+# from models.sightings import Sighting
+# load environment variables from the .env file
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Check if the database URL is set
+if not app.config['SQLALCHEMY_DATABASE_URI']:
+    raise ValueError("SQLALCHEMY_DATABASE_URI is not set.")
+
+init_db(app)
 
 @app.route('/')
+def index():
+    return('At least that worked...')
+
+@app.route('/test')
 def test_connection():
     try:
         # Perform a simple query to test the database connection
-        stmt = text('SELECT 1')
-        db.session.execute(stmt)
+        dives = Dive.query.all()
         return 'Database connection successful'
     except Exception as e:
         return f'Error connecting to database: {str(e)}'
---
+++ backend/models/database.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from flask import current_app
+
+db = SQLAlchemy()
+Base = declarative_base()
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False))
+
+def init_db(app):
+    db.init_app(app)
+    Base.query = db_session.query_property()
+    app.before_request(before_request)
+    app.teardown_request(teardown_request)
+
+def before_request():
+    db_session.configure(bind=db.engine)
+
+def teardown_request(exception=None):
+    db_session.remove()
+
+def create_all():
+    with current_app.app_context():
+        Base.metadata.create_all(bind=db.engine)
