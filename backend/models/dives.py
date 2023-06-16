a85c91d3df5368afc1b8c263053a783b45283998
Chad Nuttall <chadnuttall1@gmail.com>
2023-06-16 07:01:18 -0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   backend/models/dives.py
    
    Added logic to the Dive model to prevent duplicate dives from being
    added to the database.

--- backend/models/dives.py
+++ backend/models/dives.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import relationship, Session
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.event import listens_for
 from extensions import Base, db_session
 
 class Dive(Base):
@@ -17,3 +19,43 @@ class Dive(Base):
     def save(self, session: Session = db_session):
         session.add(self)
         session.commit()
+        
+    # Prevent duplicate entries
+    def validate_unique(self):
+        # Perform a query to check if the dive already exists
+        duplicate_dive = (
+            db_session.query(Dive)
+            .filter(
+                Dive.date == self.date,
+                Dive.dive_number == self.dive_number,
+                Dive.boat == self.boat,
+                Dive.dive_guide == self.dive_guide,
+            )
+            .first()
+        )
+        if duplicate_dive:
+            raise IntegrityError("Dive already exists")
+        
+    @staticmethod
+    @listens_for(Session, 'before_commit')
+    def before_commit(session: Session, flush_context, instances):
+        # Run the validation before committing the changes to the database
+        for instance in session.new:
+            if isinstance(instance, Dive):
+                instance.validate_unique()
+                
+        for instance in session.dirty:
+            if isinstance(instance, Dive):
+                instance.validate_unique()
+        
+    @staticmethod
+    @listens_for(Session, 'before_flush')
+    def before_flush(session: Session, flush_context, instances):
+        # Run the validation before flushing the changes to the database
+        for instance in session.new:
+            if isinstance(instance, Dive):
+                instance.validate_unique()
+
+        for instance in session.dirty:
+            if isinstance(instance, Dive):
+                instance.validate_unique()
