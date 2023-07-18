87f66a85a3d5e3decc530bdf07dff1582a842ab4
Spizeck <chadnuttall1@gmail.com>
2023-07-18 16:25:12 -0400

    On branch main Your branch is behind 'origin/main' by 2 commits, and can be fast-forwarded.   (use "git pull" to update your local branch)
    Changes to be committed:
    	modified:   .gitignore
    	modified:   frontend/package-lock.json
    	modified:   frontend/package.json
    	modified:   frontend/src/App.js
    	new file:   frontend/src/stores/RootStore.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate
-} from 'react-router-dom'
-import { useLocation } from 'react-router-dom'
+// Used for: Main app component
+import React, { useEffect, useMemo } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider, observer } from 'mobx-react'
+import RootStore from './stores/RootStore'
 import { Container, Loader, Dimmer } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
@@ -16,110 +13,28 @@ import Login from './containers/Login'
 import Register from './containers/Register'
 import ForgotPassword from './containers/ForgotPassword'
 import NotFound from './containers/NotFound.js'
-import api from './utils/api'
 
-const AppRoutes = ({ loggedIn, handleLoginSuccess }) => {
-  const navigate = useNavigate()
-  const location = useLocation()
-
-  useEffect(() => {
-    if (loggedIn && location.pathname !== '/home') {
-      navigate('/home')
-    } else if (
-      !loggedIn &&
-      !['/login', '/register', '/forgot-password'].includes(location.pathname)
-    ) {
-      navigate('/login')
-    }
-  }, [loggedIn, navigate, location.pathname])
-
+const AppRoutes = observer(() => {
   return (
     <Routes>
       <Route path='home' element={<BasePage />} />
       <Route path='register' element={<Register />} />
       <Route path='forgot-password' element={<ForgotPassword />} />
-      <Route
-        path='login'
-        element={
-          <Login handleLoginSuccess={handleLoginSuccess} loggedIn={loggedIn} />
-        }
-      />
+      <Route path='login' element={<Login />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   )
-}
+})
 
-const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [username, setUsername] = useState('')
-  const [token, setToken] = useState(localStorage.getItem('token'))
+const App = observer(() => {
+  const rootStore = useMemo(() => new RootStore(),[])
 
   useEffect(() => {
-    // Check if the user is logged in based on the token presence
-    const checkLoggedInStatus = async () => {
-      const storedToken = localStorage.getItem('token')
-      const localUsername = localStorage.getItem('username')
+    // Check if the user is logged in based on the token presence and validity
+    rootStore.checkLogin()
+  }, [rootStore])
 
-      if (storedToken) {
-        try {
-          const response = await api.getCurrentUser(storedToken)
-          if (response.status === 200) {
-            // User is logged in
-            setToken(storedToken)
-            setLoggedIn(true)
-            setUsername(
-              localUsername.charAt(0).toUpperCase() + localUsername.slice(1)
-            )
-          } else {
-            // User is not logged in
-            setToken(null)
-            localStorage.removeItem('token')
-            localStorage.removeItem('username')
-            setLoggedIn(false)
-            setUsername('')
-            window.location.reload()
-          }
-        } catch (error) {
-          // User is not logged in
-          setToken(null)
-          localStorage.removeItem('token')
-          localStorage.removeItem('username')
-          setLoggedIn(false)
-          setUsername('')
-          window.location.reload()
-        }
-      } else {
-        // User is not logged in
-        setToken(null)
-        localStorage.removeItem('token')
-        localStorage.removeItem('username')
-        setLoggedIn(false)
-        setUsername('')
-      }
-      setLoading(false)
-    }
-    checkLoggedInStatus()
-  }, [])
-
-  const handleLoginSuccess = (newToken, newUsername) => {
-    localStorage.setItem('token', newToken)
-    localStorage.setItem('username', newUsername)
-    setToken(newToken)
-    setLoggedIn(true)
-    setUsername(newUsername.charAt(0).toUpperCase() + newUsername.slice(1))
-  }
-
-  const handleLogout = () => {
-    // Remove the token from localStorage and update loggedIn state
-    localStorage.removeItem('token')
-    localStorage.removeItem('username')
-    setToken(null)
-    setLoggedIn(false)
-    window.location.reload()
-  }
-
-  if (loading) {
+  if (rootStore.loading) {
     return (
       <Dimmer active>
         <Loader>Loading...</Loader>
@@ -130,23 +45,14 @@ const App = () => {
   return (
     <Router>
       <Container>
-        <AppHeader />
-        <Navigation
-          token={token}
-          username={username}
-          setUsername={setUsername}
-          loggedIn={loggedIn}
-          handleLoginSuccess={handleLoginSuccess}
-          handleLogout={handleLogout}
-        />
-        <AppRoutes
-          loggedIn={loggedIn}
-          username={username}
-          handleLoginSuccess={handleLoginSuccess}
-        />
+        <Provider rootStore={rootStore}>
+          <AppHeader />
+          <Navigation />
+          <AppRoutes />
+        </Provider>
       </Container>
     </Router>
   )
-}
+})
 
 export default App
---
+++ frontend/src/stores/RootStore.js
@@ -0,0 +1,109 @@
+// stores/RootStore.js
+import { flow, makeAutoObservable } from 'mobx'
+import api from '../utils/api'
+
+class RootStore {
+  loading = true
+  loggedIn = false
+  username = ''
+  firstName = ''
+  preferedUnits = ''
+  token = localStorage.getItem('token') || null
+
+  constructor () {
+    makeAutoObservable(this)
+  }
+
+  checkLogin = flow(function * () {
+    if (this.token) {
+      // check if the token is valid by attempting to retrieve the user's data
+      try {
+        const response = yield api.getCurrentUser(this.token)
+        if (response.data.status === 200) {
+          this.setLoggedIn(true)
+          this.setUsername(response.data.username)
+          this.setFirstName(response.data.first_name)
+        } else {
+          this.setLoggedIn(false)
+          this.setUsername('')
+          this.setFirstName('')
+          this.setPreferedUnits('')
+          localStorage.removeItem('token')
+        }
+      } catch (error) {
+        this.setLoggedIn(false)
+        this.setUsername('')
+        this.setFirstName('')
+        this.setPreferedUnits('')
+        localStorage.removeItem('token')
+      }
+    } else {
+      this.setLoggedIn(false)
+    }
+    this.setLoading(false)
+  })
+
+  login = flow(function* (username, password) {
+    try {
+      const response = yield api.login(username, password)
+      if (response.data.status === 200) {
+        this.setLoggedIn(true)
+        this.setUsername(response.data.username)
+        this.setFirstName(response.data.first_name)
+        this.setPreferedUnits(response.data.prefered_units)
+        this.setToken(response.data.token)
+        localStorage.setItem('token', response.data.token)
+      } else {
+        this.setLoggedIn(false)
+        this.setUsername('')
+        this.setFirstName('')
+        this.setPreferedUnits('')
+        this.setToken(null)
+        localStorage.removeItem('token')
+      }
+    } catch (error) {
+      this.setLoggedIn(false)
+      this.setUsername('')
+      this.setFirstName('')
+      this.setPreferedUnits('')
+      this.setToken(null)
+      localStorage.removeItem('token')
+    }
+  })
+
+  logout () {
+    this.setLoggedIn(false)
+    this.setUsername('')
+    this.setFirstName('')
+    this.setPreferedUnits('')
+    this.token = null
+    localStorage.removeItem('token')
+  }
+
+  setLoading (value) {
+    this.loading = value
+  }
+
+  setLoggedIn (value) {
+    this.loggedIn = value
+  }
+
+  setUsername (value) {
+    this.username = value
+  }
+
+  setToken (value) {
+    this.token = value
+  }
+
+  setFirstName (value) {
+    this.firstName = value
+  }
+
+  setPreferedUnits (value) {
+    this.preferedUnits = value
+  }
+  // Additional actions and state variables can be added as needed
+}
+
+export default RootStore
